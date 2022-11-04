7b838ea7bad1b4b42c597363d69aaaaf217f1589
Mubelotix <mubelotix@gmail.com>
2022-11-04 10:23:57 +0100

    Add request interceptor

--- src/background.js
+++ src/background.js
@@ -1,1 +1,14 @@
-console.log("Wesh");
+console.log("Background script loaded");
+
+if (typeof browser === "undefined") {
+    var browser = chrome;
+}
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        let url = details.url;
+        console.log("onBeforeSendHeaders", details.url);
+    },
+    {urls: ["https://*/v1/playlist/*.m3u8"]},
+    ["requestHeaders"]
+);
