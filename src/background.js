674be19909088b00ab5fd0ea168c3921eab080ff
Mubelotix <mubelotix@gmail.com>
2022-11-04 15:14:22 +0100

    Format

--- src/background.js
+++ src/background.js
@@ -6,27 +6,24 @@ if (typeof browser === "undefined") {
 
 let dummy_sw_url = browser.runtime.getURL("dummy-sw.js");
 
-browser.declarativeNetRequest.updateSessionRules(
-    {addRules:[{
+browser.declarativeNetRequest.updateSessionRules({
+    addRules:[{
         "id": 1337,
         "priority": 1,
         "action": { "type": "redirect", "redirect": { "url": dummy_sw_url } },
         "condition": { "urlFilter": "https://static.twitchcdn.net/assets/amazon-ivs-wasmworker.min-75a2c99f45ecb5aa3225.js" },
-        }],
-       removeRuleIds: []
-    },
-)
+    }],
+    removeRuleIds: []
+});
 
 browser.declarativeNetRequest.onRuleMatchedDebug.addListener(
     (e) => {
         console.log("onRuleMatchedDebug" + JSON.stringify(e));
         if (e.request.url.startsWith("https://static.twitchcdn.net/assets/amazon-ivs-wasmworker.min")) {
-            browser.declarativeNetRequest.updateSessionRules(
-                {addRules:[],
-                   removeRuleIds: [1337]
-                },
-            )
-            
+            browser.declarativeNetRequest.updateSessionRules({
+                addRules:[],
+                removeRuleIds: [1337]
+            });
         }
     }
 );
