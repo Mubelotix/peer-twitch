28d32a50ffd2458d7147632644f7e5d8d9aa798e
Mubelotix <mubelotix@gmail.com>
2022-11-04 15:40:45 +0100

    Remove permissions

--- build.sh
+++ build.sh
@@ -2,30 +2,9 @@
 
 rm -rf pkg
 wasm-pack build --target=no-modules || exit 1
-
-# Find value of argument --manifest-version
-for arg in "$@"; do
-  case "$arg" in
-    --manifest-version=*)
-      manifest_version="${arg#*=}"
-      ;;
-  esac
-done
-
-# Copy manifest.json to pkg
-if [ "$manifest_version" = "v3" ] || [ "$manifest_version" = "3" ]; then
-  cp manifest_v3.json pkg/manifest.json
-else if [ "$manifest_version" = "v2" ] || [ "$manifest_version" = "2" ]; then
-  cp manifest_v2.json pkg/manifest.json
-else
-  echo "Packaging with manifest version v2. Pass --manifest-version=v3 to package with manifest version 3."
-  cp manifest_v2.json pkg/manifest.json
-  fi
-fi
-
+cp manifest.json pkg/manifest.json
 cp src/background.js pkg/background.js
 cp src/dummy-sw.js pkg/dummy-sw.js
-cp rules.json pkg/rules.json
 
 printf "
 const runtime = chrome.runtime || browser.runtime;
